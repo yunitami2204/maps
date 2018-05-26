0a32cc97a7f749cf79a1052a44a5d05df7c7f6c0
yunitami2204 <37372378+yunitami2204@users.noreply.github.com>
2018-05-26 22:14:25 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.364245,
+          longitude: 115.450942
+        },
+        title: 'pura di bali',
+        subtitle: 'berlokasi di rendang,karangasem'
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.415353,
+          longitude: 115.315349
+        },
+        title: 'pura di bali',
+        subtitle: 'pura tampak siring'
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.422559,
+          longitude: 115.310478
+        },
+        title: 'pura gunung kawi'
+      }
+      ,
+      {
+        key:4,
+        latlng: {
+          latitude:-8.540444,
+          longitude: 115.172515
+        },
+        title: 'pura taman ayun'
+      }
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Peta Pura di Bali
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}>  </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
